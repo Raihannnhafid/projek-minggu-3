30d7635e67367ccb99330ef505deaf5332e37f6f
RaihanHafid <raihanhafid16@gmail.com>
2025-07-23 09:39:04 +0700

    update InventoryTest

--- src/main/java/com/juaracoding/ecom/repositories/InventoryRepository.java
+++ src/main/java/com/juaracoding/ecom/repositories/InventoryRepository.java
@@ -7,7 +7,7 @@ public class InventoryRepository {
     public static By inventoryItemName = By.xpath("//div[@class='inventory_item_name']");
     public static By productSortContainer = By.xpath("//select[@class='product_sort_container']");
     public static By inventoryAddToCart = By.xpath("//button[contains(text(),'ADD TO CART')]");
-    public static By inventoryRemoveToCart = By.xpath("//button[contains(text(),'REMOVE')]");
     public static By inventoryCartBadge = By.xpath("//*[@id='shopping_cart_container']/a/span");
+    public static By inventoryRemoveToCart = By.xpath("(//button[contains(text(),'REMOVE')])[1]");
 
 }
--- src/test/java/com/juaracoding/ecom/InventoryTest.java
+++ src/test/java/com/juaracoding/ecom/InventoryTest.java
@@ -55,4 +55,25 @@ public class InventoryTest {
     Assert.assertEquals(actualTitles, expectedTitles);
     driverManager.quitDriver();
   }
+
+  @Test
+  public void addToCartTest() throws InterruptedException {
+    DriverManager driverManager = new DriverManager();
+    WebDriver driver = driverManager.getDriver();
+    driver.get("https://www.saucedemo.com/v1/index.html");
+
+    LoginUtil.performLogin(driver);
+    Thread.sleep(2000);
+
+    WebElement addBtn = driver.findElement(InventoryRepository.inventoryAddToCart);
+    addBtn.click();
+
+    WebElement cartBadge = driver.findElement(InventoryRepository.inventoryCartBadge);
+    Assert.assertEquals(cartBadge.getText(), "1");
+
+    WebElement removeBtn = driver.findElement(InventoryRepository.inventoryRemoveToCart);
+    Assert.assertEquals(removeBtn.getText(), "REMOVE");
+
+    driverManager.quitDriver();
+  }
 }
